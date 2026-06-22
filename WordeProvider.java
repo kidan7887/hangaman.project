05b19ce7472e141a3ce50f05b6b1421bff626387
kidan7887 <kidananteneh@gmail.com>
2026-06-23 01:29:00 +0300

    WordeProvider.java

--- WordeProvider.java
+++ WordeProvider.java
@@ -1,5 +1,8 @@
 package hangman.logic;
 
+// Purpose: Defines a contract for classes that provide words for the Hangman game.
 public interface WordProvider {
+
+    // Returns a word based on the selected difficulty level.
     String getWord(String difficulty);
 }
